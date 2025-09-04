371f13ece5a75b7962f1187a0121aa371d56b657
Renaildo Souza Santana <renaildos@me.local>
2025-09-04 17:07:40 -0300

    criando terraform para base mysql no aws

---
+++ terraForm-rds/main.tf
@@ -0,0 +1,110 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# --- VPC ---
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = { Name = "main-vpc" }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags   = { Name = "main-igw" }
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = { Name = "subnet-1" }
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+  tags = { Name = "subnet-2" }
+}
+
+resource "aws_route_table_association" "a1" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_route_table_association" "a2" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.rt.id
+}
+
+# --- Security Group ---
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-mysql-sg"
+  description = "Permite acesso MySQL"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "MySQL"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # devo trocar depois para meu IP!
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# --- Subnet Group ---
+resource "aws_db_subnet_group" "default" {
+  name       = "rds-subnet-group"
+  subnet_ids = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+}
+
+# --- RDS MySQL ---
+resource "aws_db_instance" "mysql" {
+  identifier              = "fiap"
+  allocated_storage       = 20
+  max_allocated_storage   = 100
+  engine                  = "mysql"
+  engine_version          = "8.0"
+  instance_class          = "db.t3.micro"
+  username                = "user_fiap"
+  password                = "pass_fiap"
+  parameter_group_name    = "default.mysql8.0"
+  skip_final_snapshot     = true
+  db_subnet_group_name    = aws_db_subnet_group.default.name
+  vpc_security_group_ids  = [aws_security_group.rds_sg.id]
+  publicly_accessible     = true
+}
+
+# --- Executar script init.sql ---
+resource "null_resource" "db_init" {
+  depends_on = [aws_db_instance.mysql]
+
+  provisioner "local-exec" {
+    command = "& \"C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin\\mysql.exe\" -h ${aws_db_instance.mysql.address} -u ${aws_db_instance.mysql.username} -p${aws_db_instance.mysql.password} -e \"source init.sql;\""
+    interpreter = ["PowerShell", "-Command"]
+  }
+}
+
+# --- Output para facilitar ---
+output "rds_endpoint" {
+  value = aws_db_instance.mysql.address
+}
