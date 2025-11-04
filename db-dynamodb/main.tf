186b1fb2826d8f097d6a81c54e39557095e40747
asilvaribeiro <asilvaribeiro@prodam.sp.gov.br>
2025-11-04 20:02:02 -0300

    Configuracao terraform para dynamodb

--- db-dynamodb/main.tf
+++ db-dynamodb/main.tf
@@ -3,10 +3,10 @@ resource "aws_dynamodb_table" "pedido_table" {
   billing_mode   = "PROVISIONED"
   read_capacity  = 20
   write_capacity = 20
-  hash_key       = "pedido_id"
+  hash_key       = "IdPedido"
 
   attribute {
-    name = "UserId"
+    name = "IdPedido"
     type = "S"
   }
 
@@ -17,6 +17,29 @@ resource "aws_dynamodb_table" "pedido_table" {
 
   tags = {
     Name        = "pedido-table-1"
+    Environment = "production"
+  }
+}
+
+resource "aws_dynamodb_table" "cliente_table" {
+  name           = "cliente"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "IdCliente"
+
+  attribute {
+    name = "IdCliente"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = true
+  }
+
+  tags = {
+    Name        = "cliente-table-1"
     Environment = "production"
   }
 }
